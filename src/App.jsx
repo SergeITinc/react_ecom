ce38c6d8ee71a428f7a57a05f91eb362b3161605
login <sergeikurankovit@gmail.com>
2022-01-31 02:25:22 +0300

    commit updade App.js

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App () {
   return (<>
     <div className="max-w-screen-xl m-auto mb-20">
       <div className={"xl:px-0 px-2"}>
-        <FeaturedTitle text={"Featured Products"} className={"py-3"} />
+        <FeaturedTitle text={"Featured Products!!!"} className={"py-3"} />
         <div className={"sm:flex justify-between items-center sm:space-x-5 space-x-0 sm:space-y-0 space-y-3"}>
           <div className={"sm:w-2/4"}>
             <div className={"aspect-w-16 aspect-h-9"}>
